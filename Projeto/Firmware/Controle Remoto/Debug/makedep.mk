898fa3acba94c00860028f2e0daf108c74c41e0d
br32108 <br32108@gmail.com>
2013-02-10 12:13:29 +0000

    Adicionado pasta Versoes com o controle das versões utilizadas no projeto. Adicionado firmware e simulação do controle remoto, parte que vai como o usuário.

---
+++ Projeto/Firmware/Controle Remoto/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ControleRemoto.c
+
+controle_def.c
+
+usart_def.c
+
